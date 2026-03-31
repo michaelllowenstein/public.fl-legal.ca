1df05c4147dc67f03ac2987c4747fce4bce1c10b
Michael Lowenstein <michaellorne@lowenstein.ca>
2026-03-30 18:03:30 -0600

    Release 3.0.3.0

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -23,5 +23,5 @@ export const env = {
     baseApiUrl: "https://api.goperigon.com/v1/all",
     baseContentUrl: "https://api.goperigon.com/v1/all?apiKey=41fe7898-8ec4-4480-9a62-c8347503c98a&q=Real%20Estate%20Legal%20in%20Canada"
   },
-  apiURL: 'https://api-fl-legal-ca.vercel.app'
+  apiURL: 'https://baremetalweb.com'
 };
