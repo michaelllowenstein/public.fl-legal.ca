ed9f79b6f6fa8ab01f7e31403715acdd25f4d49b
Michael Lowenstein <michaellorne@lowenstein.ca>
2026-04-12 04:08:55 -0600

    SMTP configurations updated.

--- server/src/services/mailer.ts
+++ server/src/services/mailer.ts
@@ -1,5 +1,10 @@
+
 import nodemailer, { Transporter, SendMailOptions } from 'nodemailer';
+import sgMail from '@sendgrid/mail';
 import { config } from '../config';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 let _transport: Transporter | null = null;
 
@@ -19,6 +24,28 @@ function transport(): Transporter {
 }
 
 const FROM = `"${config.email.fromName}" <${config.email.user}>`;
+
+function sendDefault() {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY || 'no-email-api-key');
+  // sgMail.setDataResidency('eu'); 
+  // uncomment the above line if you are sending mail using a regional EU subuser
+
+  const msg = {
+    to: 'michaelllowenstein@gmail.com',
+    from: 'support@lowenstein.ca',
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  }
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log('Email sent')
+    })
+    .catch((error) => {
+      console.error(error)
+    });
+}
 
 //                                                                              
 //  Public interfaces
