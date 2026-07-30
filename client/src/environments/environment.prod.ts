1da434b922657988e9592db2b6d380bf5cc1164e
Michael Lowenstein <iam@miloseng.com>
2026-07-30 07:28:36 -0500

    (fix) env vars exposure remediation.

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -18,5 +18,5 @@ export const env = {
     longitude: -114.06998756735986,
     latitude: 50.955083281651376,
   },
-  apiURL: 'https://fl-legal-api.up.railway.app' // or https://api.fl-legal.ca
+  apiURL: 'https:/api.fl-legal.ca.up.railway.app' // or https://api.fl-legal.ca
 };
